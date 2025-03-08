490d9824305bc973b18ee44545af12e34164636e
MJJ Cloud <mjjcloud@192.168.1.252>
2025-03-08 21:33:50 +0000

    Pushing project to GitHub

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, send_file
+
+app = Flask(__name__, template_folder='templates')  # Ensure Flask looks in 'templates/'
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/download')
+def download_file():
+    return send_file("file.pdf", as_attachment=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
