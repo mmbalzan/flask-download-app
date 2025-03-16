2d10e7dff20dfb5741f4fb2f5d79a0dacda9c24a
MJJ Cloud <mjjcloud@192.168.1.252>
2025-03-16 17:28:57 +0000

    Added logging for visits and downloads

--- app.py
+++ app.py
@@ -1,14 +1,34 @@
-from flask import Flask, render_template, send_file
+import logging
+from flask import Flask, render_template, send_file, request
+from datetime import datetime
 
-app = Flask(__name__, template_folder='templates')  # Ensure Flask looks in 'templates/'
+# Set up logging
+logging.basicConfig(filename="access.log", level=logging.INFO)
+
+app = Flask(__name__, template_folder="templates")
+
 
 @app.route('/')
 def home():
+    visitor_ip = request.remote_addr  # Get user IP
+    visit_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    # Log visit
+    logging.info(f"Home page visited from {visitor_ip} at {visit_time}")
+
     return render_template('index.html')
+
 
 @app.route('/download')
 def download_file():
+    visitor_ip = request.remote_addr  # Get user IP
+    download_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    # Log download event
+    logging.info(f"File downloaded from {visitor_ip} at {download_time}")
+
     return send_file("file.pdf", as_attachment=True, download_name="ReadMe.pdf")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
